3e0222a3e1b535b6781a37e1f27085349c8d34c1
batuaygun <batu.aygn@gmail.com>
2022-12-14 12:41:30 +0300

    done

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post("/contact", (req, res) => {
   res.render("contact");
 });
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
